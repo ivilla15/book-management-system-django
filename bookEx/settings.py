6c0088708c253bc95bd4c873a57442ef65a319c7
ivilla15 <ivilla15@calstatela.edu>
2025-09-20 20:44:25 -0700

    Add STATIC_ROOT and STATICFILES_DIRS for Render deploy

--- bookEx/settings.py
+++ bookEx/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0yiyee-f^u2-%h!**_e#h9vzz-=m^1vwzay9(!zxyrh0s_+ko7"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["book.ivilla.dev", "your-render-url.onrender.com"]
 
 
 # Application definition
@@ -128,3 +128,6 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_REDIRECT_URL = '/mybooks'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
