c48f452bf4e10bc573867c67c06c806e03169f36
ivilla15 <ivilla15@calstatela.edu>
2025-09-28 16:33:22 -0700

    Add logging config to show errors in Render logs

--- bookEx/settings.py
+++ bookEx/settings.py
@@ -142,3 +142,15 @@ STATICFILES_DIRS = [ BASE_DIR / "bookEx" / "static" ]
 LOGIN_REDIRECT_URL = '/mybooks'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
